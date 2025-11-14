d0de4440bd46c87584a842c71986cb4da4a9b5ee
sanahashim22 <sanahashim22@gmail.com>
2025-11-15 01:46:35 +0500

    Updated files

--- eval_policies.py
+++ eval_policies.py
@@ -1,9 +1,7 @@
 
 import random
 random.seed(42) 
-
 from policy_test import get_win_percentages
 from policy_random import Policy_Random
 from Policy_GreedyButOptimistic import Policy_GreedyButOptimistic
-
 print(get_win_percentages(1000, Policy_GreedyButOptimistic(), Policy_Random()))
